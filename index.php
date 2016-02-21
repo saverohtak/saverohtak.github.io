e027ced0710baf62fee5bb4e4c675a1e4d3cb5d1
Himaniworld <himaniworld@gmail.com>
2016-02-21 19:47:29 +0530

    New
    Hello

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php include("header.php");?>
+    <div class="row fullWidth raise_your_voice" >
+        <div class="columns small-10 small-offset-1">
+            <section>
+                <p>"Rohtak mercilessly squashed and burned. But no strict action taken by the government yet.. Now its time to awake the government. If you are worried about Rohtak and want to contribute, this is one possible way to wake up the officials of Rohtak. May this beautiful small-town's love, laughter and prosperity be restored.  May the angel in each of its residents be powerful enough to rub off the tears of all the ones affected. May peace be restored soon."</p>
+                <ul class="share-buttons">
+                    <li>Share This : </li>
+                    <!--<li><a href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fsaverohtak.com&t=" title="Share on Facebook" target="_blank" onclick="window.open('https://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent(document.URL) + '&t=' + encodeURIComponent(document.URL)); return false;"><img src="img/inverted/Facebook.png"></a></li>-->
+                    <div class="fb-share-button" data-href="http://saverohtak.com/" data-layout="button_count"></div>
+                    <li><a href="https://twitter.com/intent/tweet?source=http%3A%2F%2Fsaverohtak.com&text=:%20http%3A%2F%2Fsaverohtak.com" target="_blank" title="Tweet" onclick="window.open('https://twitter.com/intent/tweet?text=' + encodeURIComponent(document.title) + ':%20'  + encodeURIComponent(document.URL)); return false;"><img src="img/inverted/Twitter.png"></a></li>
+                </ul>
+            </section>
+        </div>
+    </div>
+    <div class="row fullWidth container">
+        <div class="columns small-12 medium-8"  id="letter">
+            <section class="video">
+                <iframe width="100%" height="360" src="https://www.youtube.com/embed/mJOpP3gZd8U?rel=0&autoplay=1" frameborder="0" allowfullscreen></iframe>
+            </section>
+            <section>
+                <div class="row columns small-12 medium-12 letter">
+                    <h2>Letter To C.M., Haryana</h2>
+
+                    <div>
+                        <p>To</p>
+
+                        <p>Sh. Manohar Lal Khattar<br>Chief Minister of Haryana</p>
+                        <p>Respected Sir</p>
+
+                        <p>It's been more than 7 days Jaat community of Rohtak, Haryana are on strike for Reservation under OBC. We, the normal people of Society are getting  affected by this since Feb17, 2016. People are completely house arrested because rioters are roaming on roads with weapons in their hands,  destroying properties, burning vehicles and also beating up people badly. They've even burnt and looted most of the shops in the city. We have left nothing to feed ourselves or our families. We are afraid to move out of our houses.  Markets are closed and even we are not getting medicines and Medical Aid. Many offices are also destroyed and burned by Rioters in front of local police officials, and yet no action has been taken against them. With all this happening in City, there is no police on roads now and we are under the threat of life due to all this.
+                        </p>
+
+                        <p>I Request you on behalf of people of Haryana, to solve this matter as soon as possible, So that we can live peacefully.
+                        </p>
+
+                        <p>Thank You</p>
+
+                        <p>Concerned Citizen</p>
+                    </div>
+                </div>
+            </section>
+        </div>
+      <?php require("sidebar.php");?>
+    </div>
+
+<?php include("footer.php")?>
